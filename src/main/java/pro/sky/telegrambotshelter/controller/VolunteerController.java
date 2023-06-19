ff352d689604921a99b67fdc9d681b40d8817299
irina <irishkapos16@gmail.com>
2023-06-19 22:02:18 +0300

    controller

--- src/main/java/pro/sky/telegrambotshelter/controller/VolunteerController.java
+++ src/main/java/pro/sky/telegrambotshelter/controller/VolunteerController.java
@@ -19,42 +19,76 @@ public class VolunteerController {
     VolunteerController(VolunteerService service){
         this.service = service;
     }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception.class)
     public String handleException(Exception e) {
         return String.format("%s %s", HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
 
+    /**
+     * the method returns the latest user reports
+     */
     @GetMapping//
-    public Map<Long, Report> getFaculty() {
+    public Map<Long, Report> getAllLastReports() {
             return service.getAllLastReports();
     }
     //возвращает очеты определенного пользователя
+    /**
+     * the method returns the points of a certain user
+     * @param chatId - chat identifier
+     */
     @GetMapping("/{chatId}")
     public Collection<Report> getAllByChatId(@PathVariable("chatId") Long chatId) {
         return service.getAllByChatId(chatId);
     }
+
     //отправляет пользователю принят ли отчет
+    /**
+     * the method sends the user whether the report is accepted
+     * @param chatId - chat identifier
+     * @param isGood - is the report suitable
+     */
     @PostMapping ("/{chatId}/{isGood}")
     public String isGood(@PathVariable("chatId") Long chatId, @PathVariable("isGood") boolean isGood) {
         return service.sendMessageAboutReport(chatId, isGood);
     }
-   //отправляет чаты с определенным количеством отчетов(дней) и если больше
+
+   //отправляет чаты с определенным количеством отчетов(дней) и больше
+    /**
+     * the method sends chats with a certain number of reports (days) and more
+     * @param days - days of reports
+     */
     @PostMapping ("/send-a-days/{days}")
     public List<Long> getAllWhereCount31(@PathVariable("days") int days) {
         return service.getAllWhereCount(days);
     }
+
     //отправляем сообщение о пользователю сообщение о продлении испытательного срока на 14 дней
+    /**
+     * the method sends a message to the user about the extension of the trial period for 14 days
+     * @param chatId - chat identifier
+     */
     @PostMapping ("/{chatId}/14")
     public String DayExtension14(@PathVariable("chatId") Long chatId) {
         return service.sendMessageAboutExtension(chatId,14);
     }
+
     //отправляем сообщение о пользователю сообщение о продлении испытательного срока на 30 дней
+    /**
+     * the method sends a message to the user about the extension of the trial period for 30 days
+     * @param chatId - chat identifier
+     */
     @PostMapping ("/{chatId}/30")
     public String DayExtension30(@PathVariable("chatId") Long chatId) {
         return service.sendMessageAboutExtension(chatId, 30);
     }
+
     //если пользователь прошел испытательный срок, его можно удалить
+    /**
+     * the method sends a message to the user that he has passed the probation period and removes him from the databases
+     * @param chatId - chat identifier
+     */
     @DeleteMapping ("/probation-period/{chatId}/{isPassed}")
     public String isPassedTheProbationPeriod(@PathVariable("chatId") Long chatId, @PathVariable("isPassed") boolean isPassed) {
         return service.isPassedTheProbationPeriod(chatId, isPassed);
