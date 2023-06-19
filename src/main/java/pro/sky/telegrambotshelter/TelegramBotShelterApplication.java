8f805e7dce5c30eed29dedc857efddaaea305adc
Kirill Ivanesko <kirill.ivanesko@yandex.ru>
2023-06-19 23:45:08 +0300

    added swagger, controller

--- src/main/java/pro/sky/telegrambotshelter/TelegramBotShelterApplication.java
+++ src/main/java/pro/sky/telegrambotshelter/TelegramBotShelterApplication.java
@@ -1,11 +1,13 @@
 package pro.sky.telegrambotshelter;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition
 public class TelegramBotShelterApplication {
 
 	public static void main(String[] args) {
